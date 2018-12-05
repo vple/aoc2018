cd4290f02a1a5f8a9403e7b192acdb0e915ad6b9
Vincent Le <vince1123581321@gmail.com>
2018-12-05 07:50:20 -0500

    Day 5 solutions.

--- src/main/kotlin/day05/Day05.kt
+++ src/main/kotlin/day05/Day05.kt
@@ -7,11 +7,60 @@ import util.loadResource
  */
 
 const val INPUT_FILE = "/day05/input.txt"
+const val CAPITALIZATION_DIFF = 'a' - 'A'
 
 fun main(args: Array<String>) {
     val input = loadResource(INPUT_FILE)
+    val polymer = input.lines().first()
 
     println("Part 1")
+    println(polymer.react().count())
 
     println("Part 2")
+    println(findShortestReactedPolymerLength(polymer))
+}
+
+/**
+ * Finds the length of the shortest polymer formed by removing all instances of one unit and reacting the result.
+ */
+fun findShortestReactedPolymerLength(polymer: String): Int {
+    return ('A'..'Z')
+        .map { polymer.removeUnit(it) }
+        .map { it.react() }
+        .map { it.count() }
+        .min()!!
+}
+
+/**
+ * Returns the polymer formed by removing all instances of the given [unit] from this polymer.
+ */
+fun String.removeUnit(unit: Char): String = filterNot { areSameType(it, unit) }
+
+/**
+ * Reacts a polymer, removing any two consecutive units that are opposite polarity.
+ */
+fun String.react(): String {
+    return fold("") { acc, c ->
+        val lastChar = acc.lastOrNull()
+        when {
+            lastChar == null -> c.toString()
+            areOppositePolarity(lastChar, c) -> acc.dropLast(1)
+            else -> acc.plus(c)
+        }
+    }
+}
+
+/**
+ * Returns true if [x] and [y] are the same type (letter).
+ */
+fun areSameType(x: Char, y: Char): Boolean {
+    val sameType = (x - y) % CAPITALIZATION_DIFF == 0
+    return x.isLetter() && y.isLetter() && sameType
+}
+
+/**
+ * Returns true if [x] and [y] are the same type but have opposite polarity.
+ */
+fun areOppositePolarity(x: Char, y: Char): Boolean {
+    return areSameType(x, y) && x != y
 }
